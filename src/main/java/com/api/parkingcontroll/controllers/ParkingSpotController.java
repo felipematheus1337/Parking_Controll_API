c225bebbe67c48392c8715d8666dba2418a4e364
felipematheus1337 <lipehbr@gmail.com>
2023-01-03 12:53:11 -0300

    DTO pra entidade ParkingSpotModel, finalizado

--- src/main/java/com/api/parkingcontroll/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontroll/controllers/ParkingSpotController.java
@@ -1,6 +1,7 @@
 package com.api.parkingcontroll.controllers;
 
 
+import com.api.parkingcontroll.services.ParkingSpotServiceImpl;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/parking-spot")
 public class ParkingSpotController {
+
+    final ParkingSpotServiceImpl parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotServiceImpl parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+
 }
